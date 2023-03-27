b98a86f344b58fdf1dbbb1dddfd3dc6bd28a7c8c
Dulinag <83606238+Dulinag@users.noreply.github.com>
2023-03-26 22:53:34 -0400

    Create Email.cs

---
+++ WebApplication2/Controllers/Email.cs
@@ -0,0 +1,10 @@
+namespace WebApplication2.Models
+{
+    public class Email
+    {
+        public int EmailId { get; set; }
+        public string EmailAddress { get; set; }
+
+        public bool IsSubscribed { get; set; }
+    }
+}
